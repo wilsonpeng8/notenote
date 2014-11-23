a119ca765ebb4b92972a3e89ad2e0e7b9b3192c5
Wilson Peng <wilsonpeng8@gmail.com>
2014-11-22 21:15:40 -0800

    created note

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,6 +1,7 @@
 class NotesController < ApplicationController
-end
+before_action :find_note, only:[:show,:edit,:update,:destroy]
 def index
+  @notes = Note.all.order("created_at DESC")
 end
 def show
 end
@@ -9,6 +10,13 @@ def new
 end
 def create
   @note = Note.new(note_params)
+  if @note.save
+    redirect_to @note
+  else
+    render 'new'
+
+  end
+
 end
 def edit
 end
@@ -21,8 +29,10 @@ end
 private
 
 def find_note
+  @note = Note.find(params[:id])
 end
 
 def note_params
-  paramas.require(:note).permit(:title, :content)
+  params.require(:note).permit(:title, :content)
+end
 end
